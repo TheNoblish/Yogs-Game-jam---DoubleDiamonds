6b41cbf87147244db5bdcb6d0afff2a6f8e087ec
TheNoblish <DrNoble2.0@gmail.com>
2019-12-15 00:48:21 +0100

    gull steal and patrol

--- Yogs Game jam/Assets/Scripts/FlyingEnemy.cs
+++ Yogs Game jam/Assets/Scripts/FlyingEnemy.cs
@@ -8,37 +8,57 @@ public class FlyingEnemy : MonoBehaviour
     Animator animator;
 
     GameObject player;
-
+    GameObject package;
+    bool returned = true;
     Rigidbody2D rigidbody2D;
+    int patrolDirection = -1;
 
     Vector2 playerPosition;
 
-    bool isAttacking;
-
+    public bool isAttacking;
+    public bool gotPackage;
     public float speed;
     public float attackRange;
+    public Transform nest;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        player = GameObject.Find("Player");
+        player = GameObject.Find("Player 1");
+        package = GameObject.Find("package");
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isAttacking)
+        if (!isAttacking && !gotPackage)
         {
             animator.SetBool("isDiving", false);
-            rigidbody2D.velocity = new Vector2(-1.5f, 0) * speed;
+            rigidbody2D.velocity = new Vector2(1.5f*patrolDirection, 0) * speed;
         } else if (isAttacking)
         {
 
             animator.SetBool("isDiving", true);
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
+        else if (gotPackage)
+        {
+            animator.SetBool("isDiving", false);
+            
+                transform.position = Vector2.MoveTowards(transform.position, nest.position, speed * Time.deltaTime);
+            
+        }
+
+        if (Mathf.Abs(nest.position.x - transform.position.x) > 40 && returned)
+        {
+            patrolDirection = 1;
+            returned = false;
+        }
+        else if (Mathf.Abs(nest.position.x - transform.position.x) < 5)
+            patrolDirection = -1;
+            returned = true;
 
 
         //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
@@ -46,12 +66,29 @@ public class FlyingEnemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !isAttacking)
+        if (other.CompareTag("Player") && !isAttacking && PlayerController.isCarrying)
         {
             speed = 5;
             Debug.Log("test");
             isAttacking = true;
             gameObject.transform.Rotate(0, 0, 35);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && PlayerController.isCarrying)
+        {
+            gotPackage = true;
+            PlayerController.isCarrying = false;
+            isAttacking = false;
+        }
+        if (other.gameObject.CompareTag("nest") && gotPackage)
+        {
+            gotPackage = false;
+            animator.SetBool("isDiving", false);
+            package.transform.position = new Vector3 (nest.position.x,nest.position.y+1,0f);
+            package.GetComponent<SpriteRenderer>().enabled = true;
+            package.GetComponent<BoxCollider2D>().enabled = true;
         }
     }
 }
