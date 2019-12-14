c2b2bd6197348a62e671b56849a10046054a6841
kitsparkle <kitjchadwick@gmail.com>
2019-12-14 20:03:58 +0000

    menu sound

---
+++ Yogs Game jam/Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour
+{
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onHover()
+    {
+        Debug.Log("test");
+        text.color = Color.cyan;
+    }
+
+    public void onRelease()
+    {
+        text.color = Color.white;
+    }
+}
---
+++ Yogs Game jam/Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour
+{
+    public AudioSource buttonAudioSource;
+    public AudioClip clickSoundEffect;
+    public AudioClip hoverSoundEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickButton()
+    {
+        buttonAudioSource.PlayOneShot(clickSoundEffect);
+    }
+
+    public void HoverButton()
+    {
+        buttonAudioSource.PlayOneShot(hoverSoundEffect);
+    }
+}
