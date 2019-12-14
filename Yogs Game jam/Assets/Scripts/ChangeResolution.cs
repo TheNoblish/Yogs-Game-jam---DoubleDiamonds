6520bf69086be6cae5df5118d47a89a048f728c9
kitsparkle <kitjchadwick@gmail.com>
2019-12-14 01:37:16 +0000

    Main Menu

---
+++ Yogs Game jam/Assets/Scripts/ChangeResolution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeResolution : MonoBehaviour
+{
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int currentResolutionIndex = 0;
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
---
+++ Yogs Game jam/Assets/Scripts/Volume.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMusicVolume(float vol)
+    {
+        audioMixer.SetFloat("MusicVol", vol);
+    }
+
+    public void SetEffectsVolume(float vol)
+    {
+        audioMixer.SetFloat("EffectsVol", vol);
+    }
+}
