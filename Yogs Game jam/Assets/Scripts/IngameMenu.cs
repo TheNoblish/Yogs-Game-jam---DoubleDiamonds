e08786ab610f5367bef9316452ad6c4d60cbbd5d
kitsparkle <kitjchadwick@gmail.com>
2019-12-15 14:35:43 +0000

    menu updates

---
+++ Yogs Game jam/Assets/Scripts/IngameMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameMenu : MonoBehaviour
+{
+    public GameObject menu;
+    bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (!isOpen)
+            {
+                OpenMenu();
+                isOpen = true;
+            } else
+            {
+                CloseMenu();
+                isOpen = false;
+            }
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+    }
+}
