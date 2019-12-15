d0bbf5b7cb4645d6963ce012e41459a6b3f2fae6
kitsparkle <kitjchadwick@gmail.com>
2019-12-15 16:22:32 +0000

    seagull

---
+++ Yogs Game jam/Assets/Scripts/SeagullSounds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeagullSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip audioClip = GetRandomClip();
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
